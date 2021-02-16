7b867ce4b368d3596c4b814a85141c46f7ac67b7
Phos23 <phos23@gmail.com>
2021-02-16 22:27:16 +0000

    add objects in props

--- cafe-with-a-vue/cli-version/src/components/MenuItem.vue
+++ cafe-with-a-vue/cli-version/src/components/MenuItem.vue
@@ -1,7 +1,34 @@
 <script>
 export default {
   name: 'MenuItem',
-  props: ['addToShoppingCart', 'image', 'inStock', 'name', 'price', 'quantity'],
+  // props: ['addToShoppingCart', 'image', 'inStock', 'name', 'price', 'quantity'],
+  props: {
+    addToShoppingCart: {
+      type: Function,
+      required: true
+    },
+    image: {
+      type: Object,
+      required: true
+    },
+    inStock:{
+      type: Boolean,
+      required: true
+    },
+    name: {
+      type: String,
+      required: true
+    },
+    price: {
+      type: Number,
+      required: true
+    },
+    quantity: {
+      type: Number,
+      default: 1
+    }
+
+  },
   data() {
     return {
       onSale: false
